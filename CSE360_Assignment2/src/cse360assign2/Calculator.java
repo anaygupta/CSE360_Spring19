fc49f45ee8936a3898b80a14fb223979248cb521
anaygupta <[anaygupta.us@gmail.com]>
2019-02-25 23:25:30 -0700

    added javadoc documentation

---
+++ CSE360_Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,65 @@
+//Author: Anay Gupta
+//Class ID: 390
+//Assignment #1: In Calculator.java, basic calculator functions are outlined and implemented.  
+
+package cse360assign2;
+public class Calculator 
+{
+	//declaring instance field total
+	private int total;
+	
+	/* The default constructor sets instance field total to 0. */
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/* The getTotal method returns the current total value.
+	 * @return  the total value 
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/* The add method adds the parameter to the total variable.
+	 * @param  value  the value to add to total
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/* The subtract method subtracts the parameter from the total variable.
+	 * @param  value  the value to subtract from total
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/* The multiple method multiplies the total variable by the parameter.
+	 * @param  value  the value to multiply total by 
+	 */
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	/* The divide method divides the total variable by the parameter. 
+	 * It uses integer division.
+	 * If the parameter is zero, total is set to zero. 
+	 * @param  value  the value to divide total by 
+	 */
+	public void divide (int value) 
+	{
+		
+	}
+	
+	/* The getHistory method returns an empty string.
+	 */
+	public String getHistory () 
+	{
+		return "";
+	}
+}
